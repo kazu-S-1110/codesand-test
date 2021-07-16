a4f8094b0da2d8e6a76fc06cfe25681bb94e95f8
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-07-16 23:33:37 +0900

    今日のJavaScript復習分。 ほとんど知っているから復習。

--- src/index.js
+++ src/index.js
@@ -7,4 +7,37 @@ document.getElementById("app").innerHTML = `
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
-`;
+`; // テンプレート文字列
+const name = "kazu";
+const age = 43;
+
+console.log(`my name is ${name}, i'm ${age} old.`);
+
+// アロー関数
+// 従来の記法
+function func1(str) {
+  return str;
+}
+console.log(func1("yeee!"));
+
+const func2 = function (num) {
+  return num * 2;
+};
+console.log(func2(3));
+
+//アロー関数の記法
+const func3 = (boolean) => {
+  return boolean;
+};
+console.log(func3(true));
+
+// 分割代入
+const myProfile = {
+  name: "kazu",
+  age: 23
+};
+const message1 = `my name is ${myProfile.name}, ${myProfile.age} old.`;
+
+// オブジェクトから抜き出すことができる
+const { myName, myAge } = myProfile;
+const message2 = `my name is ${myName}, ${myAge} old.`;
